14377fff6419c7341737c69f7be20e56796f3e06
Jakub Glinka <jakubglinka9@gmail.com>
2024-08-29 11:24:37 +0200

    Add a update (PUT) endpoint

--- backend/validators.js
+++ backend/validators.js
@@ -42,7 +42,8 @@ export const validateData = async (data) => {
   if (
     !heatingSource ||
     !heatingSourcePower ||
-    heatingSourceHasGrant === undefined || heatingSourceHasGrant === null
+    heatingSourceHasGrant === undefined ||
+    heatingSourceHasGrant === null
   ) {
     return {
       isDataValid: false,
@@ -53,7 +54,8 @@ export const validateData = async (data) => {
   if (
     !waterHeatingSource ||
     !waterHeatingSourcePower ||
-    waterHeatingSourceHasGrant === undefined || waterHeatingSourceHasGrant === null
+    waterHeatingSourceHasGrant === undefined ||
+    waterHeatingSourceHasGrant === null
   ) {
     return {
       isDataValid: false,
@@ -69,25 +71,95 @@ export const validateData = async (data) => {
     }
   }
 
-  // Check if someone did submit the form with the same address
+  // Check if someone did submit the form with the same house address
   const submittedData = await prisma.submittedData.findFirst({
     where: {
-      user: {
-        email,
-      },
+      solectwo,
+      street,
+      houseNumber,
     },
   })
 
   if (submittedData) {
-    if (
-      submittedData.solectwo === solectwo &&
-      submittedData.street === street &&
-      submittedData.houseNumber === houseNumber
-    ) {
-      return {
-        isDataValid: false,
-        errorMessage: "Na podany adres został już złożony formularz przez ten adres e-mail",
-      }
+    return {
+      isDataValid: false,
+      errorMessage:
+        "Na podany adres został już złożony formularz przez ten adres e-mail",
+    }
+  }
+
+  return {
+    isDataValid: true,
+    errorMessage: "",
+  }
+}
+
+export const validateUpdateData = async (data) => {
+  const {
+    name,
+    surname,
+    email,
+
+    solectwo,
+    street,
+    houseNumber,
+
+    heatingSource,
+    heatingSourcePower,
+    heatingSourceHasGrant,
+    heatingSourceGrantYear,
+
+    waterHeatingSource,
+    waterHeatingSourcePower,
+    waterHeatingSourceHasGrant,
+    waterHeatingSourceGrantYear,
+
+    isInterested,
+    interestedInYear,
+  } = data
+  if (!name || !surname || !email) {
+    return {
+      isDataValid: false,
+      errorMessage: "Name, surname and email are required",
+    }
+  }
+
+  if (!solectwo || !houseNumber) {
+    return {
+      isDataValid: false,
+      errorMessage: "Solectwo, street and house number are required",
+    }
+  }
+
+  if (
+    !heatingSource ||
+    !heatingSourcePower ||
+    heatingSourceHasGrant === undefined ||
+    heatingSourceHasGrant === null
+  ) {
+    return {
+      isDataValid: false,
+      errorMessage: "Heating source, power and grant information are required",
+    }
+  }
+
+  if (
+    !waterHeatingSource ||
+    !waterHeatingSourcePower ||
+    waterHeatingSourceHasGrant === undefined ||
+    waterHeatingSourceHasGrant === null
+  ) {
+    return {
+      isDataValid: false,
+      errorMessage:
+        "Water heating source, power and grant information are required",
+    }
+  }
+
+  if (!isInterested) {
+    return {
+      isDataValid: false,
+      errorMessage: "Interested information is required",
     }
   }
 
